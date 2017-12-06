67ab4f77011fec976c6977b4f5d6ce4728d94d24
Vladimir <postdev85@gmail.com>
2017-12-06 21:27:20 +0200

    lesson-11-tasks 1-2

--- src/lesson-11/tasks/example-1.js
+++ src/lesson-11/tasks/example-1.js
@@ -1,12 +1,41 @@
 import fetch from 'isomorphic-fetch';
 
-fetch('//offline-news-api.herokuapp.com/stories')
-    .then(function(response) {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
+const resultList = [];
+const promises = [];
+const pathList = ['https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=cn',
+    'https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=ca',
+    'https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=au'];
+
+function fillResultList(sources) {
+    for (const { name, description, country } of sources) {
+        const item = {
+            name,
+            description,
+            country
         }
-        return response.json();
-    })
-    .then(function(stories) {
-        console.log(stories);
+        resultList.push(item);
+    }
+}
+
+for (const path of pathList) {
+    const prom = fetch(path)
+        .then(function (response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+        .then(function ({ sources }) {
+            fillResultList(sources);
+        });
+    promises.push(prom);
+}
+
+Promise.all(promises).then(
+    () => {
+        console.log('Successful');
+        console.log(resultList);
+    },
+    () => {
+        console.log('Error getting all data');
     });
--- src/lesson-11/tasks/example-2.js
+++ src/lesson-11/tasks/example-2.js
@@ -1,12 +1,43 @@
 import fetch from 'isomorphic-fetch';
 
-fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=886faf57284140dd830bf5d6ddadd32f')
-    .then(function(response) {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
+const resultList = [];
+const promises = [];
+const pathList = ['https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=cn',
+    'https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=ca',
+    'https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=au'];
+
+function fillResultList(sources) {
+    for (const { name, description, country } of sources) {
+        const item = {
+            name,
+            description,
+            country
         }
-        return response.json();
-    })
-    .then(function(stories) {
-        console.log(stories);
-    });
+        resultList.push(item);
+    }
+}
+
+async function getData(path) {
+    return fetch(path)
+        .then(function (response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+        .then(function ({ sources }) {
+            fillResultList(sources);
+        });
+}
+
+(async () => {
+    try {
+        const cn = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=cn');
+        const ca = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=ca');
+        const au = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=au');
+        console.log(resultList);
+    }
+    catch (error) {
+        console.log(error);
+    }
+})();
