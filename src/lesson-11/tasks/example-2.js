89983ad4efe686bf13ad5eb37d7ae62ac6b20beb
Vladimir <postdev85@gmail.com>
2017-12-08 09:39:54 +0200

    1

--- src/lesson-11/tasks/example-2.js
+++ src/lesson-11/tasks/example-2.js
@@ -14,7 +14,7 @@ function fillResultList(sources) {
     }
 }
 
-async function getData(path) {
+function getData(path) {
     return fetch(path)
         .then(function (response) {
             if (response.status >= 400) {
@@ -32,9 +32,9 @@ async function getData(path) {
 
 (async () => {
     try {
-        const cn = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=cn');
-        const ca = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=ca');
-        const au = await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=au');
+        await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=cn');
+        await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=ca');
+        await getData('https://newsapi.org/v2/sources?apiKey=f51575f85f6f45a39506856c4eb46563&country=au');
         console.log(resultList);
     }
     catch (error) {
